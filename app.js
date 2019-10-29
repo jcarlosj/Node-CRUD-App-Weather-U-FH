839ba05b79f46f5923ef4623f4a5dd42edf7d1a0
Juan Carlos Jiménez Gutierrez <jcjimenez29@misena.edu.co>
2019-10-29 18:21:36 -0500

    [+] JSON: Estructura de datos de respuesta
    Aclaracion:
    La ejecucion de la aplicacion se hace por linea de comandos de manera que la
    sintaxis es la siguiente:
    
       node app -d "<nombre-lugar>"
    
    Ejemplo:
    
       node app -d "Medellin Colombia"

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ axios .get( `http://api.geoapify.com/v1/geocode/search?text=${ encodedUrl }&limi
     .then( response => {
         //console .log( response );
         console .log( response .status );
-        console .log( response .data );
+        console .log( JSON .stringify( response .data, undefined, 2 ) );    /**  JSON .stringify: Convierte el Objeto en un String permitiendo ver toda la estructura de datos */
     })
     .catch( err => console .error( 'ERROR!!!', err ) );
