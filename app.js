34b78c622c2dbb1098f8923e1214810c64f54993
Juan Carlos Jiménez Gutierrez <jcjimenez29@misena.edu.co>
2019-10-29 18:17:36 -0500

    NPM: Instala e Implementa Axios y realiza peticion HTTP GET al API GeoAPIfy

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const argv = require( './config/yargs' ) .argv;
+const 
+    axios = require( 'axios' ),
+    argv = require( './config/yargs' ) .argv;
 
 /** Al ejecutar: node app -d "Bogota Colombia" */
 console .log( argv .direccion );    // Bogota Colombia
 
+let encodedUrl = encodeURI( argv .direccion );      /** encodeURI() Reemplaza todos los caracteres excepto los siguientes con las secuencias de escape UTF-8 */
+
+/** Promise: Petición get usando Axios e implementa la API https://www.geoapify.com/ */
+axios .get( `http://api.geoapify.com/v1/geocode/search?text=${ encodedUrl }&limit=1&type=city&apiKey=9825d1acfb7948a683e77381150f8694` )
+    .then( response => {
+        //console .log( response );
+        console .log( response .status );
+        console .log( response .data );
+    })
+    .catch( err => console .error( 'ERROR!!!', err ) );
