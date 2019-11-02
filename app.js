57251bb77e70dfc76d103877b40904c5419906fc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-11-02 10:56:42 -0500

    [+] Axios > HTTP Get: Obtener Temperatura a partir de Latitud y Longitud

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const 
     argv = require( './config/yargs' ) .argv,
-    place = require( './controllers/placeController' );
+    place = require( './controllers/placeController' ),
+    weather = require( './controllers/weatherController' );
 
 /** Al ejecutar: node app -d "Bogota Colombia" */
 console .log( argv .direccion );    // Bogota Colombia
 
 /** Promesa: Obtiene datos ciudad */
-place .getPlaceLatLng( argv .direccion )
-    .then( data => {
-        console .log( data );
-    })
-    .catch( err => console .log( 'Error', err ) );
+// place .getPlaceLatLng( argv .direccion )
+//     .then( data => {
+//         console .log( data );
+//     })
+//     .catch( err => console .log( 'Error', err ) );
 
+weather .getWeatherByLatLng( 3.4517923, -76.5324943 ) 
+    .then( data => {
+        console .log( 'Temperatura', data .temperatura );
+    }) 
+    .catch( err => console .log( 'Error', err ) );
---
+++ controllers/weatherController.js
@@ -0,0 +1,21 @@
+const axios = require( 'axios' );
+
+const getWeatherByLatLng = async ( lat, lng ) => {
+    let 
+        apiKey = '73083ce69cc77ea705c8f19db51ac73b',
+        response = await axios .get ( `https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&units=metric&appid=${ apiKey }` );
+
+    console .log( 'Data OpenWeather', response .data .main );
+    
+    return { 
+        temperatura: response .data .main .temp, 
+        presion: response .data .main .pressure, 
+        humedad: response .data .main .humidity, 
+        temperatura_minima: response .data .main .temp_min, 
+        temperatura_maxima: response .data .main .temp_max 
+    };
+}
+
+module .exports = {
+    getWeatherByLatLng
+}
